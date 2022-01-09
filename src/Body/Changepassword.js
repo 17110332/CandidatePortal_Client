0b258d54735e90ebb6a8a522bfa8747621a76b8a
17110332 <17110332@student.hcmute.edu.vn>
2022-01-09 13:13:52 +0700

    send mail

--- src/Body/Changepassword.js
+++ src/Body/Changepassword.js
@@ -5,6 +5,7 @@ import {Link} from "react-router-dom";
  import 'react-toastify/dist/ReactToastify.css';  
  import Listconst from './../Const/Listconst';
  import axios from 'axios';
+ import {decode as base64_decode, encode as base64_encode} from 'base-64';
 const APIstr = Listconst.API;
  toast.configure();
 
@@ -14,16 +15,68 @@ class Changepassword extends Component{
     {
         super(props);
         this.state={
-            Email:"",
             Username:"",
             Password:"",
             Newpassword:"",
             Newpasswordconfirm:""
         }
     }
+    onChange =(e)=>{
+      var target = e.target;
+      var name = target.name;
+      var value = target.type=="checkbox" ? target.checked : target.value;
+      this.setState({
+        [name]: value
+      });
+    }
+    ChangePassword=()=>{
+        let{Username,Password,Newpasswordconfirm,Newpassword} = this.state;
+        if(Newpasswordconfirm != Newpassword)
+        {
+          toast.warning("Mật khậu nhập không khớp");
+          return;
+        }
+        else if(Username =="" || Password=="" || Newpassword=="" || Newpasswordconfirm=="")
+        {
+          toast.warning("Các thông tin không được để trống!");
+          return;
+        }
+        let formdata = new FormData();
+        formdata.set("Username",Username);
+        formdata.set("Password",base64_encode(Password));
+        formdata.set("NewPassword",base64_encode(Newpassword));
+        axios.post(APIstr +`api/AccountAction/ChangePassword`,formdata)
+        .then(res=>{
+                if(res && res.data==0)
+                {
+                  toast.error('Sai tài khoản hoặc mật khẩu!')
+                  return;
+                }
+                if(res && res.data==-1)
+                {
+                  toast.error('Call API unsuccess!')
+                  return;
+                }
+                else
+                {
+                  toast.success("Đổi mật khẩu thành công!");
+                  this.setState({
+                    Username:"",
+                    Password:"",
+                    Newpassword:"",
+                    Newpasswordconfirm:""
+                  })
+                }
+        })
+        .catch(err=>{
+            toast.error('Call API unsuccess!')
+            return;
+        })
+
+    }
     render()
     {
-        let{Email,Username,Password,Newpasswordconfirm,Newpassword} = this.state
+        let{Username,Password,Newpasswordconfirm,Newpassword} = this.state
         return(
             <div className="login-main">
           <div className="w-login m-auto">
@@ -43,25 +96,20 @@ class Changepassword extends Component{
                   </div>
                   <div className="input-div one">
                     <div className="div lg-lable">
-                      <input placeholder="Nhập email lúc tạo tài khoản" type="text" className="input form-control-lgin"    name="Email" value={Email}  onChange={this.onChange}/>
+                      <input placeholder="Mật khẩu cũ" type="password" className="input form-control-lgin"    name="Password" value={Password}  onChange={this.onChange}/>
                     </div>
                   </div>
                   <div className="input-div one">
                     <div className="div lg-lable">
-                      <input placeholder="Mật khẩu cũ" type="text" className="input form-control-lgin"    name="Password" value={Password}  onChange={this.onChange}/>
+                      <input placeholder="Mật khẩu mới" type="password" className="input form-control-lgin"    name="Newpassword" value={Newpassword}  onChange={this.onChange}/>
                     </div>
                   </div>
                   <div className="input-div one">
                     <div className="div lg-lable">
-                      <input placeholder="Mật khẩu mới" type="text" className="input form-control-lgin"    name="Newpassword" value={Newpassword}  onChange={this.onChange}/>
+                      <input placeholder="nhập lại khẩu mới" type="password" className="input form-control-lgin"    name="Newpasswordconfirm" value={Newpasswordconfirm}  onChange={this.onChange}/>
                     </div>
                   </div>
-                  <div className="input-div one">
-                    <div className="div lg-lable">
-                      <input placeholder="nhập lại khẩu mới" type="text" className="input form-control-lgin"    name="Newpasswordconfirm" value={Newpasswordconfirm}  onChange={this.onChange}/>
-                    </div>
-                  </div>
-                  <button type="button" className="btn btn-primary float-right btn-login d-block w-100" onClick={()=>this.Regist()}>
+                  <button type="button" className="btn btn-primary float-right btn-login d-block w-100" onClick={()=>this.ChangePassword()}>
                     Đổi mật khẩu
                   </button>
                 </form>
--- src/Body/Forgotpassword.js
+++ src/Body/Forgotpassword.js
@@ -18,6 +18,45 @@ class Forgotpassword extends Component{
             Username:""
         }
     }
+    onChange =(e)=>{
+      var target = e.target;
+      var name = target.name;
+      var value = target.type=="checkbox" ? target.checked : target.value;
+      this.setState({
+        [name]: value
+      });
+    }
+    SendPassword = ()=>{
+      let{Username,Email} = this.state;
+      if(Username =="" || Email=="")
+      {
+        toast.warning("Các thông tin không được để trống!");
+        return;
+      }
+      let formdata = new FormData();
+      formdata.set("Username",Username);
+      formdata.set("Email",Email);
+      axios.post(APIstr +`api/AccountAction/ForgotPassword`,formdata)
+      .then(res=>{
+              if(res && res.data==-1)
+              {
+                  toast.error('Không gửi được mail do chính sách bảo mật! Liên hệ trực tiếp quản trị viên để lấy lại mật khẩu')
+                  return;
+              }
+              else
+              {
+                  toast.success("Đã gửi mật khẩu qua mail, vui lòng kiểm tra mail!");
+                  this.setState({
+                    Username:"",
+                    Email:""
+                  })
+              }
+      })
+      .catch(err=>{
+          toast.error('Call API unsucess')
+          return;
+      })
+    }
     render()
     {
         let{Email,Username} = this.state
@@ -43,7 +82,7 @@ class Forgotpassword extends Component{
                       <input placeholder="Nhập email lúc tạo tài khoản" type="text" className="input form-control-lgin"    name="Email" value={Email}  onChange={this.onChange}/>
                     </div>
                   </div>
-                  <button type="button" className="btn btn-primary float-right btn-login d-block w-100" onClick={()=>this.Regist()}>
+                  <button type="button" className="btn btn-primary float-right btn-login d-block w-100" onClick={()=>this.SendPassword()}>
                     Gửi mật khẩu mới tới email
                   </button>
                 </form>
